51d4246cec7754449f329ecc5b6dbcf5df43a910
mrxcode <mrxcode.dev@gmail.com>
2025-03-06 19:54:21 +0630

    email fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -23,6 +25,14 @@ class User extends Authenticatable
         'password',
     ];
 
+
+    // my modi code
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@springwaveservices.com') && $this->hasVerifiedEmail();
+    }
+
+    // end my code
     /**
      * The attributes that should be hidden for serialization.
      *
