b40ff2caad5e38924633cdba6c3d645a995d970c
mrxcode <mrxcode.dev@gmail.com>
2025-03-10 18:14:35 +0000

    user add to filament

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\PasswordInput;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+               TextInput::make('name')
+                ->required()
+                ->label('Name'),
+            TextInput::make('email')
+                ->required()
+                ->email()
+                ->unique(ignoreRecord: true)
+                ->label('Email'),
+            TextInput::make('password')
+                ->required()
+                ->password() // Use password method here
+                ->label('Password')
+                ->dehydrateStateUsing(fn ($state) => bcrypt($state)),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('id')->sortable()->searchable(),
+            TextColumn::make('name')->sortable()->searchable(),
+            TextColumn::make('email')->sortable()->searchable(),
+            TextColumn::make('created_at')->sortable(),
+            TextColumn::make('updated_at')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
