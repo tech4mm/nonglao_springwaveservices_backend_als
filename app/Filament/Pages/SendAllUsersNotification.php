e53956cc68c9d4c4802886b04b1fd8c183fbe868
Mr Xcode <mrxcode.dev@gmail.com>
2025-03-23 18:11:26 +0630

    send noti ui fix

--- app/Filament/Pages/SendAllUsersNotification.php
+++ app/Filament/Pages/SendAllUsersNotification.php
@@ -15,12 +15,13 @@ class SendAllUsersNotification extends Page implements HasForms
 {
     use InteractsWithForms;
 
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-oval-left-ellipsis';
+    protected static ?string $navigationGroup = 'Admin Panel';
+    protected static ?int $navigationSort = 99;
     protected static string $view = 'filament.pages.send-all-users-notification';
 
     public ?string $notificationTitle = null;
     public ?string $notificationBody = null;
-    // public ?string $notificationImage = null;
     public string|array|null $notificationImage = null;
 
     public function mount(): void
@@ -45,31 +46,6 @@ class SendAllUsersNotification extends Page implements HasForms
     protected function getFormModel(): \Illuminate\Database\Eloquent\Model|string|null{
         return User::class;
     }
-
-    // public function send(): void
-    // {
-    //     $tokens = User::whereNotNull('fcm_token')->pluck('fcm_token')->toArray();
-
-    //     foreach ($tokens as $token) {
-    //         Http::withToken(env('FCM_SERVER_KEY'))->post('https://fcm.googleapis.com/fcm/send', [
-    //             'to' => $token,
-    //             'notification' => [
-    //                 'title' => $this->notificationTitle,
-    //                 'body' => $this->notificationBody,
-    //                 // 'image' => $this->notificationImage ?? null,
-    //                 'image' => $this->notificationImage ? asset('storage/' . $this->notificationImage) : null,
-    //             ],
-    //             'data' => [
-    //                 'click_action' => 'FLUTTER_NOTIFICATION_CLICK',
-    //             ],
-    //         ]);
-    //     }
-
-    //     Notification::make()
-    //         ->title('Notification sent to all users')
-    //         ->success()
-    //         ->send();
-    // }
 
     public function send(): void{
         $tokens = User::whereNotNull('fcm_token')->pluck('fcm_token')->toArray();
--- resources/views/filament/pages/send-all-users-notification.blade.php
+++ resources/views/filament/pages/send-all-users-notification.blade.php
@@ -2,9 +2,18 @@
     <form wire:submit.prevent="send">
         {{ $this->form }}
 
-        <div class="mt-6"> {{-- Add margin-top here --}}
-            <x-filament::button type="submit">
-                Send Notification
+        <div class="mt-6">
+            <x-filament::button
+                type="submit"
+                wire:loading.attr="disabled"
+                wire:target="notificationImage"
+            >
+                <span wire:loading.remove wire:target="notificationImage">
+                    Send Notification
+                </span>
+                <span wire:loading wire:target="notificationImage">
+                    Uploading Image...
+                </span>
             </x-filament::button>
         </div>
     </form>
