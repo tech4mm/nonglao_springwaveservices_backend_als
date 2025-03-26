bbb6fb7b44e8991999a297d3280e0e188b4b3c55
Mr Xcode <mrxcode.dev@gmail.com>
2025-03-27 04:40:07 +0630

    company detail factory fixed

--- database/factories/CompanyDetailsFactory.php
+++ database/factories/CompanyDetailsFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CompanyDetails>
  */
-class CompanyDetailFactory extends Factory
+class CompanyDetailsFactory extends Factory
 {
     /**
      * Define the model's default state.
