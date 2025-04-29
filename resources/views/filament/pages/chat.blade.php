65135b764150823e0827ab873e69de29cd442b8e
mrxcode <mrxcode.dev@gmail.com>
2025-04-29 14:23:11 +0000

    chat send icon changed

--- resources/views/filament/pages/chat.blade.php
+++ resources/views/filament/pages/chat.blade.php
@@ -275,12 +275,13 @@
                     <button wire:click="sendMessage"
                         class="bg-blue-600 p-2 rounded-full hover:bg-blue-700 transition duration-150"
                         title="Send">
-                        <svg class="w-5 h-5 transform rotate-45" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <!-- <svg class="w-5 h-5 transform rotate-45" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" 
                             stroke-linejoin="round" 
                             stroke-width="2" 
                             d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
-                        </svg>
+                        </svg> -->
+                        ==>
                     </button>
                 </div>
             </div>
