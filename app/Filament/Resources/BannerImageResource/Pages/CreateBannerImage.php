be765fe982afb2067b63c00617eceb352cc8d37f
mrxcode <mrxcode.dev@gmail.com>
2025-04-29 10:53:29 +0000

    banner image create and update go back to list

--- app/Filament/Resources/BannerImageResource/Pages/CreateBannerImage.php
+++ app/Filament/Resources/BannerImageResource/Pages/CreateBannerImage.php
@@ -3,10 +3,20 @@
 namespace App\Filament\Resources\BannerImageResource\Pages;
 
 use App\Filament\Resources\BannerImageResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBannerImage extends CreateRecord
 {
     protected static string $resource = BannerImageResource::class;
+
+    protected function getCreatedNotification(): ?\Filament\Notifications\Notification
+    {
+        return \Filament\Notifications\Notification::make()
+            ->title('Created successfully')
+            ->success();
+    }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
